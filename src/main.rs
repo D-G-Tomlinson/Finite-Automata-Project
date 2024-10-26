d958ce60baad0d20bb3bdfb454d4e4f16691d856
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2024-10-26 12:03:12 +0100

    read arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Location of the file storing the DSA
+    #[arg(short,long)]
+    dfa: Option<String>,
+
+    /// The word to be analysed
+    #[arg(short, long, value_name = "FILE")]
+    word: Option<PathBuf>,
+}
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(dfa) = cli.dfa.as_deref() {
+        println!("Value for dfa: {dfa}");
+    }
 }

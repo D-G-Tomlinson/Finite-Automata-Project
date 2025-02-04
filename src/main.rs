d7c6d6540b2b5613b9bc564b9a378d019eabe547
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-02-04 14:59:15 +0000

    adjusted run_dfa, more decomposition

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,6 @@ struct Cli {
     nfa_output: Option<String>,
 
 }
-
-struct DFAState {
-    transitions: Vec<usize>,
-    accepting:bool
-}
 #[derive(Debug, PartialEq, Eq)]
 enum InputType{
     DFA,
@@ -82,13 +77,13 @@ fn main() {
     let output_nfa_in = cli.nfa_output.as_deref().map(|s| s.to_string());
     
     let result:Rslt = match input_type {
-	InputType::DFA => run_dfa(lines, input_word),
+	InputType::DFA => dfa_option(lines, input_word),
 	InputType::NFA => run_nfa(lines, input_word, output_dfa),
 	InputType::REGEX => run_regex(regex_in, output_dfa, output_nfa_in, input_word)
     };
     
     println!("{}", match result {
-	Rslt::Err(e) => e,
+	Rslt::Err(e) => format!("Program failed! The following error was produced: \n{}",e),
 	Rslt::Acc => format!("ACCEPT"),
 	Rslt::Rej => format!("REJECT"),
 	Rslt::Nop => format!("No word provided, program finished without computation, only conversion."),
@@ -120,9 +115,7 @@ fn read_input_file(address:&str) -> Result<(InputType, Vec<String>),String> {
 	_ => {
 	    return Err(format!("File type is unsupported."));
 	}
-    }
-
-    
+    }    
     let mut contents= String::new();
     match File::open(address) {
 	Ok(mut f) => _ = f.read_to_string(&mut contents),
@@ -134,16 +127,95 @@ fn read_input_file(address:&str) -> Result<(InputType, Vec<String>),String> {
     let lines = contents.lines().map(|s| s.to_string()).collect();
     return Ok((input_type,lines));
 }
-/*
-fn dfa_option(lines:Vec<&str>,input_word:Option<String>) -> Rslt {
 
-}*/
+struct DFA {
+    states: Vec<DFAState>,
+    alphabet_map: HashMap<char,usize>,
+    starting:usize
+}
 
-fn run_dfa(lines:Vec<String>, input_word:Option<String>) -> Rslt {
-   
-    if lines.len()<3 {
-	return Rslt::Err(format!("Input file is too short"));
+struct DFAState {
+    transitions: Vec<usize>,
+    accepting:bool
+}
+
+fn dfa_option(lines:Vec<String>, input_word:Option<String>) -> Rslt {
+    let dfa:DFA;
+    match lines_to_dfa(lines) {
+	Err(e) => return Rslt::Err(e),
+	Ok(d) => dfa = d
     }
+
+    return run_dfa(dfa,input_word);
+}
+
+fn lines_to_dfa(lines:Vec<String>) -> Result<DFA,String> {
+    if lines.len()<3 {
+	return Err(format!("Input file is too short"));
+    }
+    
+    let alphabet = &lines[0];
+    let mut alphabet_map:HashMap<char,usize> = HashMap::new();
+
+    let mut i:usize = 0;
+    for letter in alphabet.chars() {
+	if alphabet_map.contains_key(&letter) {
+	    return Err(format!("The alphabet contains a duplicate letter"));
+	} else {
+	    alphabet_map.insert(letter,i);
+	    i = i + 1;
+	}
+    }
+
+    let starting = lines[1].parse::<usize>().unwrap()-1;   
+    
+    let num_states = lines.len()-2;
+    let num_letters = alphabet.len();
+    let mut states:Vec<DFAState> = Vec::new();
+    
+    for line in &lines[2..] {
+	match line_to_dfa_state(line,num_letters,num_states) {
+	    Err(e) => return Err(e),
+	    Ok(new_state)=> states.push(new_state)
+	}
+    }
+    return Ok(DFA{states,alphabet_map,starting});
+
+}
+fn line_to_dfa_state(line:&String,num_letters:usize,num_states:usize) -> Result<DFAState,String> {
+    let split_state:Vec<&str> = line.split(",").collect();
+    if split_state.len() != num_letters+1 {
+	return Err(format!("Invalid number of elements on line"));
+    }
+	
+    let mut next_states:Vec<usize> = Vec::new();
+    for next_state_str in (&split_state[0..num_letters]).into_iter(){
+	match next_state_str.parse::<usize>() {
+	    Ok(next_state_num) => {
+		if next_state_num >= 1 && next_state_num <= num_states {
+		    next_states.push(next_state_num-1)
+		} else {
+		    return Err(format!("Value of next state is outside of the bounds of possible states"));
+		}
+	    },
+	    Err(_) => return Err(format!("Value for next state is not a valid number")),
+	}
+    }
+    
+    let accept:bool;
+    match split_state[num_letters].parse() {
+	Ok(a) => accept = a,
+	Err(_) => return Err(format!("Poorly formatted accepting/not accepting value.")),
+    }
+    
+    return Ok(
+	DFAState{
+	    transitions: next_states,
+	    accepting: accept}
+    );
+}
+fn run_dfa(dfa:DFA, input_word:Option<String>) -> Rslt {
+   
     let word:String;
     if let Some(in_word) = input_word {
 	word=in_word;
@@ -151,69 +223,20 @@ fn run_dfa(lines:Vec<String>, input_word:Option<String>) -> Rslt {
 	return Rslt::Notodo;
     }
     let word = word.as_str();
-    
-    let alphabet = &lines[0];
-    let mut alphabet_map:HashMap<char,usize> = HashMap::new();
 
-    let mut i:usize = 0;
-    for letter in alphabet.chars() {	
-	alphabet_map.insert(letter,i);
-	i = i + 1;
-    }
-
-    let num_states = lines.len()-2;
-    
-    let mut current_state=lines[1].parse::<usize>().unwrap()-1;
-
-    let num_letters = alphabet.len();
-    let mut states:Vec<DFAState> = Vec::new();
-    
-    for state in &lines[2..] {
-	let split_state:Vec<&str> = state.split(",").collect();
-
-	if split_state.len() != num_letters+1 {
-	    return Rslt::Err(format!("Invalid number of elements on line"));
-	}
-	
-	let mut next_states:Vec<usize> = Vec::new();
-	for ns in (&split_state[0..num_letters]).into_iter(){
-	    match ns.parse::<usize>() {
-		Ok(v) => {
-		    if v >= 1 && v <= num_states {
-			next_states.push(v-1)
-		    } else {
-			return Rslt::Err(format!("Value of next state is outside of the bounds of possible states"));
-		    }
-		},
-		Err(_) => return Rslt::Err(format!("Value for next state is not a valid number")),
-	    }
-	}//.map(|n| n.parse::<usize>().unwrap()-1).collect();
-
-	let accept:bool;
-	match split_state[num_letters].parse() {
-	    Ok(a) => accept = a,
-	    Err(_) => return Rslt::Err(format!("Poorly formatted accepting/not accepting value.")),
-	}
-	
-	let new_state = DFAState{
-	    transitions: next_states,
-	    accepting: accept
-	}; 
-	states.push(new_state);
-    }
-
+    let mut current_state=dfa.starting;
     for letter in word.chars() {
 
-	if !alphabet_map.contains_key(&letter) {
+	if !dfa.alphabet_map.contains_key(&letter) {
 	    return Rslt::Rej;//if a letter in the word is not in the alphabet, reject the word
 	}	
-	let equivalent = alphabet_map[&letter];
-	let current_state_obj = &states[current_state];
+	let equivalent = dfa.alphabet_map[&letter];
+	let current_state_obj = &dfa.states[current_state];
 	let edges = &current_state_obj.transitions;
 	current_state = edges[equivalent];
     }
 //    println!{"Final state is {}",current_state}
-    return match states[current_state].accepting {
+    return match dfa.states[current_state].accepting {
 	true => Rslt::Acc,
 	false => Rslt::Rej
     }

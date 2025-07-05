6c31dc5ebc96e83305fdd27d7514495f44ba7a6c
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-07-06 00:51:11 +0100

    convert input nfa string to nfa object

--- src/nfa.rs
+++ src/nfa.rs
@@ -6,29 +6,117 @@ use crate::dfa::DFAState;
 #[derive(Clone)]
 pub struct NFAState {
     transitions:Vec<Vec<usize>>,
-    accepting:bool
+    accepting:bool,
 }
 
 impl NFAState {
     pub fn new(transitions:Vec<Vec<usize>>,accepting:bool) -> NFAState {
 		return NFAState{transitions,accepting};
     }
+	pub fn from_line(line:&String,alphabet:&HashMap<char,usize>,num_states:usize) -> Result<NFAState,String> {
+		let comma_split:Vec<&str> = line.split(",").collect();
+		let num_parts = comma_split.len();
+		let mut transitions:Vec<Vec<usize>> = vec![Vec::new();alphabet.len()+1]; // +1 because of the empty letter, the jump, is not in the alphabet but does have transitions
+
+		if num_parts > 1 {
+			for transition in (&comma_split[0..num_parts-1]).into_iter() {
+				let parts:Vec<&str> = transition.split(":").collect();
+				match parts.len() {
+					0 | 1 => return Err(format!("Each transition must have a ':'")),
+					2 => (),
+					_ => return Err(format!("Each transition must have only one ':'")),
+				};
+
+				// get letter
+				let chars:Vec<char> = parts[0].chars().collect();
+				let transition_num:usize = match chars.len() {
+					0 => 0,
+					1 => {
+						if alphabet.contains_key(&chars[0]) {
+							alphabet[&chars[0]] as usize
+						} else {
+							return Err(format!("The transition letter must be in the alphabet"));
+						}
+					},
+					_ => return Err(format!("Each transition must have one or no letters, not multple"))
+				};
+
+				// get next state
+				let value:usize = match parts[1].parse::<usize>() {
+					Ok(n) => n,
+					Err(_) => return Err(format!("Next state must be a number"))
+				};
+				if value>num_states {
+					 return Err(format!("The next state value is too big"));
+				} else if value == 0 {
+					return Err(format!("The next state cannot be zero, states are indexed from 1"))
+				}
+				if !transitions[transition_num].contains(&value) {
+					transitions[transition_num].push(value);
+				}
+			}
+		}
+		let accepting:bool = match comma_split[num_parts -1].parse() {
+			Ok(a) => a,
+			Err(_)  => return Err(format!("Poorly formatted accepting value"))
+			
+		};
+		return Ok(NFAState::new(transitions,accepting));
+	}
 }
 
 pub struct NFA {
 	states:Vec<NFAState>,
 	starting:usize,
-	alphabet:String
+	alphabet:HashMap<char,usize>
 }
 
 impl NFA {
-	pub fn new(states:Vec<NFAState>,starting:usize,alphabet:String) -> NFA {
+	pub fn new(states:Vec<NFAState>,starting:usize,alphabet:HashMap<char,usize>) -> NFA {
 		return NFA{states, starting, alphabet};
 	}
 	pub fn from_lines(lines:Vec<String>) -> Result<NFA,String> {
-		return Err(format!("not finished yet"));
+		let num_lines = lines.len();
+		if num_lines < 3 {
+			return Err(format!("Input file is too short"))
+		}
+		let alphabet = NFA::get_alphabet_hm(&lines[0]);
+
+		let starting = match lines[1].parse::<usize>() {
+			Ok(n) => {
+				if n <= num_lines - 2 && n>0 {
+					n
+				} else {
+					return Err(format!("Starting number is not suitable"))
+				}
+			},
+			Err(_) => return Err(format!("Starting is not a number"))
+		};
+		let state_lines = &lines[2..];
+		let mut states:Vec<NFAState> = Vec::new();
+
+		for line in state_lines{
+			states.push(match NFAState::from_line(line,&alphabet,num_lines-2) {
+				Ok(nfastate) => nfastate,
+				Err(e) => return Err(e)
+			});
+	}
+		return Ok(NFA::new(states,starting,alphabet));
 	}
 
+	fn get_alphabet_hm(alphabet:&str) -> HashMap<char,usize> {
+		let alphabet:Vec<char> = alphabet.chars().collect();
+		let mut alphabet_hashmap = HashMap::<char,usize>::new();
+		let mut i = 1; //0 represents the jump, which is the empty letter
+		for c in &alphabet {
+			if !alphabet_hashmap.contains_key(c) {
+				alphabet_hashmap.insert(*c,i);
+				i = i + 1;
+			}
+		}
+		return alphabet_hashmap;
+	}
+	
 	pub fn run(&self,input_word:Option<&str>, output_dfa:Option<&str>) -> Rslt {
 		return Rslt::Err(format!("not finished yet"));
 	}

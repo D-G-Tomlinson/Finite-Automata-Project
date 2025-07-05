9efd6082244287f2fdd27ef145f2c7dc80e27c99
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-07-05 22:17:09 +0100

    started redoing nfa

--- src/dfa.rs
+++ src/dfa.rs
@@ -10,53 +10,81 @@ pub struct DFA {
 
 impl DFA {
     pub fn new(states:Vec<DFAState>, alphabet_map:HashMap<char,usize>,starting:usize) -> DFA {
-	return DFA{states, alphabet_map, starting};
+		return DFA{states, alphabet_map, starting};
     }
     
     pub fn run(&self, word:&str) -> Rslt {
-	
-	let mut current_state=self.starting;
-	for letter in word.chars() {
-	    
-	    if !self.alphabet_map.contains_key(&letter) {
-		return Rslt::Rej;//if a letter in the word is not in the alphabet, reject the word
-	    }	
-	    let equivalent = self.alphabet_map[&letter];
-	    let current_state_obj = &self.states[current_state];
-	    let edges = &current_state_obj.transitions;
-	    current_state = edges[equivalent];
-	}
-//    println!{"Final state is {}",current_state}
-	return match self.states[current_state].accepting {
-	    true => Rslt::Acc,
-	    false => Rslt::Rej
-	}
-	
+		
+		let mut current_state=self.starting;
+		for letter in word.chars() {
+			
+			if !self.alphabet_map.contains_key(&letter) {
+				return Rslt::Rej;//if a letter in the word is not in the alphabet, reject the word
+			}	
+			let equivalent = self.alphabet_map[&letter];
+			let current_state_obj = &self.states[current_state];
+			let edges = &current_state_obj.transitions;
+			current_state = edges[equivalent];
+		}
+		//    println!{"Final state is {}",current_state}
+		return match self.states[current_state].accepting {
+			true => Rslt::Acc,
+			false => Rslt::Rej
+		}
+		
     }
     pub fn format(&self) -> Vec<String> {
-	let mut output:Vec<String> = Vec::new();
-	output.push(DFA::format_alphabet(&self.alphabet_map));
-	output.push((self.starting+1).to_string());
-	for state in &self.states {
-	    let mut line=String::new();
-	    for transition in &state.transitions {
-		line.push_str(&format!("{},",transition+1));
+		let mut output:Vec<String> = Vec::new();
+		output.push(DFA::format_alphabet(&self.alphabet_map));
+		output.push((self.starting+1).to_string());
+		for state in &self.states {
+			let mut line=String::new();
+			for transition in &state.transitions {
+				line.push_str(&format!("{},",transition+1));
 	    }
-	    line.push_str(&state.accepting.to_string());
-	    output.push(line);
-	}
-    return output;
+			line.push_str(&state.accepting.to_string());
+			output.push(line);
+		}
+		return output;
     }
     
     fn format_alphabet(alphabet_map:&HashMap<char,usize>) -> String {
-	let mut chars:Vec<char> = vec!['a';alphabet_map.len()];
-    for k in alphabet_map.keys() {
-	chars[alphabet_map[k]] = *k;
-    }
-	return chars.into_iter().collect();
+		let mut chars:Vec<char> = vec!['a';alphabet_map.len()];
+		for k in alphabet_map.keys() {
+			chars[alphabet_map[k]] = *k;
+		}
+		return chars.into_iter().collect();
     }
     
     
+	fn from_lines(lines:Vec<String>) -> Result<DFA,String> {
+		if lines.len()<3 {
+			return Err(format!("Input file is too short"));
+		}
+		
+		let alphabet = &lines[0];
+		let alphabet_map;
+		match alphabet_to_alphabet_map(alphabet) {
+			Ok(am) => alphabet_map = am,
+			Err(e) => return Err(e)
+		}
+		
+		let starting = lines[1].parse::<usize>().unwrap()-1;   
+		
+		let num_states = lines.len()-2;
+		let num_letters = alphabet.len();
+		let mut states:Vec<DFAState> = Vec::new();
+		
+		for line in &lines[2..] {
+			match DFAState::from_line(line,num_letters,num_states) {
+				Err(e) => return Err(e),
+				Ok(new_state)=> states.push(new_state)
+			}
+		}
+		return Ok(DFA::new(states,alphabet_map,starting));
+		
+}
+
 }
 
 #[derive(Clone)]
@@ -67,53 +95,58 @@ pub struct DFAState {
 
 impl DFAState {
     pub fn new(transitions:Vec<usize>,accepting:bool) -> DFAState {
-	return DFAState{transitions, accepting};
+		return DFAState{transitions, accepting};
     }
+	fn from_line(line:&String,num_letters:usize,num_states:usize) -> Result<DFAState,String> {
+		let split_state:Vec<&str> = line.split(",").collect();
+		if split_state.len() != num_letters+1 {
+			return Err(format!("Invalid number of elements on line"));
+		}
+		
+		let mut next_states:Vec<usize> = Vec::new();
+		for next_state_str in (&split_state[0..num_letters]).into_iter(){
+			match next_state_str.parse::<usize>() {
+				Ok(next_state_num) => {
+					if next_state_num >= 1 && next_state_num <= num_states {
+						next_states.push(next_state_num-1)
+					} else {
+						return Err(format!("Value of next state is outside of the bounds of possible states"));
+					}
+				},
+				Err(_) => return Err(format!("Value for next state is not a valid number")),
+			}
+		}
+		
+		let accept:bool;
+		match split_state[num_letters].parse() {
+			Ok(a) => accept = a,
+			Err(_) => return Err(format!("Poorly formatted accepting/not accepting value.")),
+		}
+		
+		return Ok(
+			DFAState::new(
+				next_states,
+				accept)
+		);
+	}
+
 }
 
 pub fn dfa_option(lines:Vec<String>, input_word:Option<&str>) -> Rslt {
     
     let word:&str;
     if let Some(in_word) = input_word {
-	word=in_word;
+		word=in_word;
     } else {
-	return Rslt::Notodo;
+		return Rslt::Notodo;
     }
-
+	
     let dfa:DFA;
-    match lines_to_dfa(lines) {
-	Ok(d) => dfa = d,	
-	Err(e) => return Rslt::Err(e)
+    match DFA::from_lines(lines) {
+		Ok(d) => dfa = d,	
+		Err(e) => return Rslt::Err(e)
     }
     return dfa.run(word);
-}
-
-fn lines_to_dfa(lines:Vec<String>) -> Result<DFA,String> {
-    if lines.len()<3 {
-	return Err(format!("Input file is too short"));
-    }
-    
-    let alphabet = &lines[0];
-    let alphabet_map;
-    match alphabet_to_alphabet_map(alphabet) {
-	Ok(am) => alphabet_map = am,
-	Err(e) => return Err(e)
-    }
-
-    let starting = lines[1].parse::<usize>().unwrap()-1;   
-    
-    let num_states = lines.len()-2;
-    let num_letters = alphabet.len();
-    let mut states:Vec<DFAState> = Vec::new();
-    
-    for line in &lines[2..] {
-	match line_to_dfa_state(line,num_letters,num_states) {
-	    Err(e) => return Err(e),
-	    Ok(new_state)=> states.push(new_state)
-	}
-    }
-    return Ok(DFA::new(states,alphabet_map,starting));
-
 }
 
 pub fn alphabet_to_alphabet_map(alphabet:&str) -> Result<HashMap<char,usize>, String> {
@@ -131,35 +164,3 @@ pub fn alphabet_to_alphabet_map(alphabet:&str) -> Result<HashMap<char,usize>, St
     return Ok(alphabet_map);
 }
 
-fn line_to_dfa_state(line:&String,num_letters:usize,num_states:usize) -> Result<DFAState,String> {
-    let split_state:Vec<&str> = line.split(",").collect();
-    if split_state.len() != num_letters+1 {
-	return Err(format!("Invalid number of elements on line"));
-    }
-	
-    let mut next_states:Vec<usize> = Vec::new();
-    for next_state_str in (&split_state[0..num_letters]).into_iter(){
-	match next_state_str.parse::<usize>() {
-	    Ok(next_state_num) => {
-		if next_state_num >= 1 && next_state_num <= num_states {
-		    next_states.push(next_state_num-1)
-		} else {
-		    return Err(format!("Value of next state is outside of the bounds of possible states"));
-		}
-	    },
-	    Err(_) => return Err(format!("Value for next state is not a valid number")),
-	}
-    }
-    
-    let accept:bool;
-    match split_state[num_letters].parse() {
-	Ok(a) => accept = a,
-	Err(_) => return Err(format!("Poorly formatted accepting/not accepting value.")),
-    }
-    
-    return Ok(
-	DFAState::new(
-	    next_states,
-	    accept)
-    );
-}
